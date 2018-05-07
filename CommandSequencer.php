e676ffe9c1334d47df6eca92e17c3c04147deffa
Andrey Chekmarev <assminog@gmail.com>
2018-05-07 19:58:43 +0300

    initial

---
+++ CommandSequencer.php
@@ -0,0 +1,28 @@
+class CommandSequencer
+        {
+        private $config=[
+              'baseFolder'=>'/home/zenitron-user/scripts/';
+<------>];
+<------>public function _construct($command,$dataType='array(int)')   // CommandSequencer(MakeAgeless,'array('int')');' //command=class
+<------><------>{
+<------><------>$this->data=$this->commandStartController($command);
+<------><------>$this->$command=$Command=new $command;
+<------><------>$this->data=$this->stopController($command,$dataType);
+<------><------>}
+<------>private function startCantroller($command)
+<------><------>{
+<------><------>include$this->config['baseFolder'].ucfirst($command).'.php';
+<------><------>//include classes
+<------><------>//
+<------><------>}
+<------>private function stopControoler()
+<------><------>{
+<------><------>$this->clearMetods();
+<------><------>$this->clearPropsExept($prop);
+<------><------>}
+<------>private function clearMethods()
+<------><------>{
+<------><------>$metudsArray=get_class_methods();
+<------><------>foreach
+<------><------>}
+<------>}
