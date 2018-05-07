af88b678126c5a0b015e5e3a00fa893e5369b5cc
Andrey Chekmarev <assminog@gmail.com>
2018-05-07 20:00:45 +0300

    clearing

--- CommandSequencer.php
+++ CommandSequencer.php
@@ -2,27 +2,27 @@ class CommandSequencer
         {
         private $config=[
               'baseFolder'=>'/home/zenitron-user/scripts/';
-<------>];
-<------>public function _construct($command,$dataType='array(int)')   // CommandSequencer(MakeAgeless,'array('int')');' //command=class
-<------><------>{
-<------><------>$this->data=$this->commandStartController($command);
-<------><------>$this->$command=$Command=new $command;
-<------><------>$this->data=$this->stopController($command,$dataType);
-<------><------>}
-<------>private function startCantroller($command)
-<------><------>{
-<------><------>include$this->config['baseFolder'].ucfirst($command).'.php';
-<------><------>//include classes
-<------><------>//
-<------><------>}
-<------>private function stopControoler()
-<------><------>{
-<------><------>$this->clearMetods();
-<------><------>$this->clearPropsExept($prop);
-<------><------>}
-<------>private function clearMethods()
-<------><------>{
-<------><------>$metudsArray=get_class_methods();
-<------><------>foreach
-<------><------>}
-<------>}
+        ];
+        public function _construct($command,$dataType='array(int)')   // CommandSequencer(MakeAgeless,'array('int')');' //command=class
+                {
+                $this->data=$this->commandStartController($command);
+                $this->$command=$Command=new $command;
+                $this->data=$this->stopController($command,$dataType);
+                }
+        private function startCantroller($command)
+                {
+                include$this->config['baseFolder'].ucfirst($command).'.php';
+                //include classes
+                //
+                }
+        private function stopControoler()
+                {
+                $this->clearMetods();
+                $this->clearPropsExept($prop);
+                }
+        private function clearMethods()
+                {
+                $metudsArray=get_class_methods();
+                foreach
+                }
+        }
