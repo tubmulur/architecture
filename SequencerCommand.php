12bc2636e43b8e964bd5122844f56206321a37ec
Andrey Chekmarev <assminog@gmail.com>
2020-03-21 09:47:43 +0300

    Update SequencerCommand.php

--- SequencerCommand.php
+++ SequencerCommand.php
@@ -13,15 +13,17 @@ I have no warranty,
 that my account was not used
 by someone, I don't know.
 
-Sorry for not cotrolled account.
-Sources will be checked and validated as
-Soon as possible.
+Sorry for this "out of control 
+period" of my account.
+
+My sources will be checked, validated 
+and repaired as Soon as possible.
 
 In this file there were a silly joke,
 not desttuctive or dangerous for me
 or someone else.
 
 Sorry for that. Account will be protected
-safer.
+better.
 09:17 Sat 21 mart 2020
 Andrey A.Chekmarev #3464
