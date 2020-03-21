5e2aa7deeaef34125f5bb700fb1ef4cc8aa39294
Andrey Chekmarev <assminog@gmail.com>
2020-03-21 09:19:46 +0300

    Andrey A. Chekmarev, important account update.

--- SequencerCommand.php
+++ SequencerCommand.php
@@ -1,2 +1,17 @@
-Everything deleted here. I have no warranty, that my account was not used by someone. Sorry for not cotrolled account.
+This file was deleted.
+I understand, that I must exit,
+from my github account, before I
+exiting the office.
+Today I discovered some code 
+in this file, that was not
+written by myself.
+
+For today 09:15, I have no warranty, 
+that my account was not used 
+by someone, I don't knew. 
+
+Sorry for not cotrolled account.
+Sources will be checked and validated as
+Soon as possible.
+09:17 SUT 21 mart 2020
 Andrey A.Chekmarev
