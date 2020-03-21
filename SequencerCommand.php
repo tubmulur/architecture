b532d94b2485ec11a55ae35be524ea9af42d1ab6
Andrey Chekmarev <assminog@gmail.com>
2020-03-21 09:37:51 +0300

    Important update, grammar fix

--- SequencerCommand.php
+++ SequencerCommand.php
@@ -1,14 +1,16 @@
 This file was deleted.
 I understand, that I must exit,
 from my github account, before I
-exiting the office, but I think I don't
-Today I discovered some code
+exiting the office, but I think I wasn't
+perfect with it.
+
+So today I discovered some code
 in this file, that was not
 written by myself.
 
 For today 09:15, I have no warranty,
 that my account was not used
-by someone, I don't knew.
+by someone, I don't know.
 
 Sorry for not cotrolled account.
 Sources will be checked and validated as
