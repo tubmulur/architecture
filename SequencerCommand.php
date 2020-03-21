1ac53dbbe5c5a09a77919fd8948cf1227b71ce2e
Andrey Chekmarev <assminog@gmail.com>
2020-03-21 09:44:05 +0300

    Update SequencerCommand.php

--- SequencerCommand.php
+++ SequencerCommand.php
@@ -6,14 +6,22 @@ perfect with it.
 
 So today I discovered some code
 in this file, that was not
-written by myself.
+written by me.
 
-For today 09:15, I have no warranty,
+For today 09:15 Sat 21 March 2020.
+I have no warranty,
 that my account was not used
 by someone, I don't know.
 
 Sorry for not cotrolled account.
 Sources will be checked and validated as
 Soon as possible.
-09:17 SUT 21 mart 2020
+
+In this file there were a silly joke,
+not desttuctive or dangerous for me
+or someone else.
+
+Sorry for that. Account will be protected
+safer.
+09:17 Sat 21 mart 2020
 Andrey A.Chekmarev #3464
